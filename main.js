f4e7bf2e02c66420a1ae9368eb78cb5f98ca9422
Denise Onukwue <donukwue1@gmail.com>
2021-03-24 13:28:39 -0400

    Add JS MVC thoughts

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
+
+///MODEL/////
 /*----- constants -----*/
+// variables that are not going to be reassigned 
+
 /*----- app's state (variables) -----*/
+//changes that are being tracked /data at a given moment in time
+
+
+
+
 /*----- cached element references -----*/
+//html elements to be manipulated 
+
 /*----- event listeners -----*/
-/*----- functions -----*/x
+//button to play game 
+
+//CONTROLLER//
+
+/*----- functions -----*/
+function initialize(){
+//intialze our data to start the game 
+}
+
+function playerOptions(){}
+function turn (){}
+function finalResults(){}
+function winner(){}
+function render( ){}
