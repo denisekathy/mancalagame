d6f8fff97c73e2ce345d76acffb9a60115920b86
Denise Onukwue <donukwue1@gmail.com>
2021-03-24 19:35:48 -0400

    Update playerTurn functions start and intiliaze

--- main.js
+++ main.js
@@ -2,70 +2,86 @@
 ///MODEL/////
 /*----- constants -----*/
 // variables that are not going to be reassigned 
-const hole;
-const pit;
-const player1 = //Player 1
-const player2 = //Player 2
-
-
+let board = [0 ,4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];
+const playerTurn = {
+    p1: "Player1",
+    p2: "Player2"
+};
 
 /*----- app's state (variables) -----*/
 //changes that are being tracked /data at a given moment in time
 
-let scores, 
-let holes,
-let pits, 
-let winner 
+// let scores, 
+// let holes,
+// let pits, 
+// let winner 
 
 
 /*----- cached element references -----*/
 //html elements to be manipulated 
 
-const p1scoreEl = document.querySelector("#player1-score");
-const p2scoreEl = document.querySelector("#player2-score");
-const hole  = document.querySelectorAll(".cole hole");
+const p1ScoreEl = document.querySelector("#player1-score");
+const p2ScoreEl = document.querySelector("#player2-score");
+const hole  = document.querySelectorAll(".col hole");
 const pit = document.querySelectorAll(".mancala");
 
+const p1ResultsEl = document.querySelector("#player1-score");
+const p2ResultsEl = document.querySelector("player2-score");
+
+document.querySelectorAll(".col hole").addEventListener
+("click", start);
 
 /*----- event listeners -----*/
-//button to play game 
-
-
-
+//for the holes
+document.querySelectorAll(".col hole").addEventListener
+("click", start);
 /*----- functions -----*/
 function initialize(){
+    scores = {
+        p1: 0,
+        p2: 0,
+    };
+
+    playerTurn= null;
+    
+
+    winner = null;
+    render()
+
+}
 //intialze our data to start the game 
-let board = [0 ,4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];
-let playerTurn = {
-    p1: "Player1",
-    p2: "Player2"
-};
+
 //CONTROLLER// 
 
 //How we will decide whose turn it is 
-function whoseTurn(){
-    if (idx >= 6 && idx !== 0){
+//Possibly a foreach function
+//possibly turn this function whoseTurn into a class and place in playerOption function 
+board.forEach(function (whoseTurn, idx){
+    if (idx > 0 && idx <= 6){
+        playerTurn = 1;
         return playerTurn
     }
-}
+     else {
+         playerTurn = -1;
+        return playerTurn
+     }
+    });
 
-function player2Turn(){
-    (idx =< 13 && idx !== 7)
-}
-function changeTurn (){
-    if ( variable for player1 !=== pit) {
-     return player1Turn
+function playerOption(){
+    if (idx > 0 && idx >= 6){
+       //allow player to pick these options 
     }
-    else 
-        (variable for player1 === pit ){
-            return player2Turn
-        }
-     
+     else {
+        return playerTurn.p2
+     }
+    };
+
+function start() {
+      playerTurn = 1;
     
+     render()
 }
-
-function playerOption(){}
-
+    
 function finalResult(){}
 
 function winner(){}
@@ -73,6 +89,14 @@ function winner(){}
 //VIEW 
 function board(){}
 function hole(){}
-function render( ){}
- //this function will be called whenever the state is changed 
- //changes the apps state  
+function render( ){
+  p1ScoreEl.textContent = scores.p1;
+  p2ScoreEl.textContent = scores.p2;
+
+  //do I need to render the intial game board titled let board or based on my results
+
+}
+
+
+ initialize();
+ render();
