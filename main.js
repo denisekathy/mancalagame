9597fb9194a1ca69d433a1b7241ab4282fc4be0d
Denise Onukwue <donukwue1@gmail.com>
2021-03-25 16:52:06 -0400

    Add new pseudocode and restructured game

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-
 ///MODEL/////
 /*----- constants -----*/
-// variables that are not going to be reassigned 
-let board = [0 ,4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];
+// variables that are not going to be reassigned
+let board = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];
 const player1 = {
-    holes: [1, 2, 3, 4, 5, 6],
-    pit : [0]
+  holes: [1, 2, 3, 4, 5, 6],
+  pit: [0],
 };
 
 const player2 = {
-    holes: [8, 9, 10, 11, 12, 13],
-    pit: [7]
+  holes: [8, 9, 10, 11, 12, 13],
+  pit: [7],
 };
+
 
 let playerHoles = 14;
 /*----- app's state (variables) -----*/
@@ -22,183 +22,146 @@ let playerHoles = 14;
 // let pits = 0;
 // let winner = null;
 
-
 /*----- cached element references -----*/
-//html elements to be manipulated 
+//html elements to be manipulated
 
 const p1ScoreEl = document.querySelector("#player1-score");
 const p2ScoreEl = document.querySelector("#player2-score");
-const holes  = document.querySelectorAll(".hole"); //need to confirm this is the right syntax
+const holes = document.querySelectorAll(".hole"); //need to confirm this is the right syntax
 const pit = document.querySelectorAll(".mancala");
 
 const p1ResultsEl = document.querySelector("#player1-score");
 const p2ResultsEl = document.querySelector("player2-score");
 
-const hole1 = document.getElementById("#h2");
-const hole2 = document.getElementById("#h3");
-const hole3 = document.getElementById("#h3");
-const hole4 = document.getElementById("#h4");
-const hole5 = document.getElementById("#h5");
-const hole6 = document.getElementById("#h6");
-const hole7 = document.getElementById("#h7");
-const hole8 = document.getElementById("#h8");
-const hole9 = document.getElementById("#h9");
-const hole10 = document.getElementById("#h10");
-const hole11 = document.getElementById("#h11");
-const hole12 = document.getElementById("#h12");
+const hole0= document.getElementById("0");
+const hole1= document.getElementById("1");
+const hole2= document.getElementById("2");
+const hole3= document.getElementById("3");
+const hole4= document.getElementById("4");
+const hole5= document.getElementById("5");
+const hole6= document.getElementById("6");
+const hole7= document.getElementById("7");
+const hole8= document.getElementById("8");
+const hole9 = document.getElementById("9");
+const hole10 = document.getElementById("10");
+const hole11 = document.getElementById("11");
+const hole12 = document.getElementById("12");
+const hole13= document.getElementById("13");
 
 /*----- event listeners -----*/
 //for the holes
-holes.forEach(hole =>
-    hole.addEventListener("click", start) );
+holes.forEach((hole) => hole.addEventListener("click", start));
 
 /*----- functions -----*/
+function initialize() {
+  scores = {
+    p1: 0,
+    p2: 0,
+  };
 
-function initialize(){
-    scores = {
-        p1: 0,
-        p2: 0,
-    };
+  // Amount of tokens in each hole at the beginning//
+  let holes = 4; // 4 marbles per hole
+  let pits = 0;
 
-    // Amount of tokens in each hole at the beginning// 
-    let holes = 4; // 4 marbles per hole 
-    let pits = 0;
-
-    playerTurn = null;
-    
- let boardStart = function(){
-     for ( let i = 0; i < board; i++){ 
-        if( i == player1.pit || i == player2.pit){
-            board[i]= 0; 
-        }
-        else {
-            board[i] = holes;
-        }
- };
-
- let pickUpMarbles = funtion(startIndex){
-     var i = 0;
-     for (i = 0; i < playerHoles; i++) {
-         let aHole = i + startIndex;
-
-        //  if(startIndex === player2) {   //not sure if this function is needed 
-        //      aHole = board.length - 2 - i;
-        //  }
-         let amountOfMarbles = board[aHole];
-     }
- }
-    winner = null;
-    render()
-
+  playerTurn = null;
+  winner = null;
+  render();
 }
 
-
-//How we will decide whose turn it is 
-//Possibly a foreach function
-//possibly turn this function whoseTurn into a class and place in playerOption function 
-// board.forEach(function (boar, idx){
-//     if(boar === player1.holes > 0 && player1.holes <= 6){
-//         return start;
-//     }
-//      else {
-//          return player2.holes
-//     });
-
-
-let playerOption = function(){
-    let option = parseInt();
-
-    if (option[holes] === 0) {
-
-    }
-    else if (holeSelected(option, player1.pit)){
-        if (gameOver()) }
-        //find a way to see if the game is over 
-    }
-
-    else {
-        player.2 turn
-    }
+function pickUpMarbles(){
+// Player 1 goes first and can only pick up marbles from indexes 1-6 and not indexes 0 or 7
+//Player 2 can only pick up marbles from indexes 8-13 and not indexes 0 or 7
 }
-    
-};
 
-// // Decide wha
-// function playerOption(){
-//     if (idx > 0 && idx >= 6){
-       
-//     }
-//      else {
-//         return playerTurn.p2
-//      }
-//     };
+function depositMarbles{
+//Based on the number of pickedUpMarbles, deposit one marble into each hole starting from indexes 1-6 except at index 7 for Player 1 
+}//Based on the number of pickedUpMarbles, deposit one marble into each hole  starting from indexes 8-13 except at index 0 for Player 2 
+//Do both of these in a counter-clockwise fashion
+
+function whoseTurn(){
+    //If player 1's marbles last land in hole with an index of 0, repeat turn 
+    //If player 1's marbles last land in holes with indexes of 8-13, then it is Player 2's turn
+    //If player 2's marbles last land in a hole with an index of 7, repeat turn 
+    //If player 2's marbles last land in holes with indexes of 1-6, then in is Player 2's turn
+} 
 
 
 
 function start(evt) {
-    let holeSelected = evt.target;
-    let holeSelectedIndex = player1.holes;
-    
-   let holeSelected = function(startIndex, pits){
-       let i = 0;
-       let amountOfMarbles = board[startIndex]; // Amount of tokes in the selected bin
-       let repeat = 0;
-       let aHole = 0;
+//   let holeSelected = evt.target;
+//   let holeSelectedIndex = parseInt(holeSelected.id);
+  
 
-       board[startIndex] = 0;
-       
-    //Loop around the holes"arrays" beginning with the one selected and the amount picked up 
-    for ( i = startIndex + 1; i < startIndex + 1 + amountOfMarbles; i++) {
-        aHole = i % playerHoles; //go around board
-        board[aHole] = board[aHole} + 1; // board[Hole] += 1
-    }
-      if( aHole === player1.pit) {
-          let repeat = true;
-          return repeat;
-      }
 
-    
-   }
-   
-
-     render()
-}
-    
-// function finalResult(){}
-
-function winner(){
-    let i = 0;
-    let player1.pit = 0;
-    let player2.pit = 1;
-    
-    for(i = 0; i < 6; i++){
-        if ( player1.pit > player2.pit){
-            alert("Player 1 Wins");
-        }
-        else {
-            alert("Player 2 Wins");
-        }
-    }
+  render();
 }
 
-//VIEW 
-// // allows user to click on a hole on their side 
+function getWin() {
+  // Compare player 1 score to player 2 score and the larger amount will be the winner
+  //Prompts with who is the winner and who is the loser 
+}
+
+
+function render() {
+  p1ScoreEl.textContent = scores.p1;
+  p2ScoreEl.textContent = scores.p2;
+}
+
+initialize();
+render();
+
+
+//VIEW
+// // allows user to click on a hole on their side
 // function selectHole( board, player){
-//     return 
+//     return
 // }
 
-// //allows the distribution of the marbles 
+// //allows the distribution of the marbles
 // function hole(){
 //    if (playerTurn){
 //        for (let i = 1; i < 7; i++)
 //    }
 // }
-function render( ){
-  p1ScoreEl.textContent = scores.p1;
-  p2ScoreEl.textContent = scores.p2;
+//   let holesSelected = function (holeSelectedIndex, pits) {
+//     let i = 0;
+//     let amountOfMarbles = board[startIndex]; 
+//     let repeat = 0;
+//     let aHole = 0;
 
-}
+    
+//     board[holeSelectedIndex] = 0;
+
+//     //Loop around the holes"arrays" beginning with the one selected and the amount picked up
+//     for (let i = holeSelectedIndex + 1; i < holeSelectedIndex + 1 + amountOfMarbles; i++) {
+//       aHole = i % playerHoles; //go around board
+//       board[aHole] = board[aHole] + 1; // board[Hole] += 1
+//     }
+//     if (aHole === player1.pit) {
+//       let repeat = true;
+//       return repeat;
+//     }
+   
+// };
 
 
- initialize();
- render();
+// let boardStart = function () {
+//   for (let i = 0; i < board; i++) {
+//     if (i == player1.pit || i == player2.pit) {
+//       board[i] = 0;
+//     } else {
+//       board[i] = holes;
+//     }
+//   }
+// };
+
+
+// let pickUpMarbles = function (startIndex) {
+//   for (let i = 0; i < playerHoles; i++) {
+//     let aHole = i + startIndex;
+//     let amountOfMarbles = board[aHole];
+
+//   }
+// }
+ 
+// function finalResult(){}
