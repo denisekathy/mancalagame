b4a52375590eebb80d80e1131a8de50783ba0b1c
Denise Onukwue <donukwue1@gmail.com>
2021-03-25 03:00:19 -0400

    Start, foreach updates. cache elements update

--- main.js
+++ main.js
@@ -3,11 +3,15 @@
 /*----- constants -----*/
 // variables that are not going to be reassigned 
 let board = [0 ,4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];
-const playerTurn = {
-    p1: "Player1",
-    p2: "Player2"
+const player1 = {
+    holes: [1, 2, 3, 4, 5, 6],
+    pit : [0]
 };
 
+const player2 = {
+    holes: [8, 9, 10, 11, 12, 13],
+    pit: [7]
+};
 /*----- app's state (variables) -----*/
 //changes that are being tracked /data at a given moment in time
 
@@ -22,29 +26,40 @@ const playerTurn = {
 
 const p1ScoreEl = document.querySelector("#player1-score");
 const p2ScoreEl = document.querySelector("#player2-score");
-const hole  = document.querySelectorAll(".col hole");
+const holes  = document.querySelectorAll(".hole"); //need to confirm this is the right syntax
 const pit = document.querySelectorAll(".mancala");
 
 const p1ResultsEl = document.querySelector("#player1-score");
 const p2ResultsEl = document.querySelector("player2-score");
 
-document.querySelectorAll(".col hole").addEventListener
-("click", start);
+const hole1 = document.getElementById("#h2");
+const hole2 = document.getElementById("#h3");
+const hole3 = document.getElementById("#h3");
+const hole4 = document.getElementById("#h4");
+const hole5 = document.getElementById("#h5");
+const hole6 = document.getElementById("#h6");
+const hole7 = document.getElementById("#h7");
+const hole8 = document.getElementById("#h8");
+const hole9 = document.getElementById("#h9");
+const hole10 = document.getElementById("#h10");
+const hole11 = document.getElementById("#h11");
+const hole12 = document.getElementById("#h12");
 
 /*----- event listeners -----*/
 //for the holes
-document.querySelectorAll(".col hole").addEventListener
-("click", start);
+holes.forEach(hole =>
+    hole.addEventListener("click", start) );
 /*----- functions -----*/
+
 function initialize(){
     scores = {
         p1: 0,
         p2: 0,
     };
 
-    playerTurn= null;
+    playerTurn = null;
     
-
+ 
     winner = null;
     render()
 
@@ -56,29 +71,31 @@ function initialize(){
 //How we will decide whose turn it is 
 //Possibly a foreach function
 //possibly turn this function whoseTurn into a class and place in playerOption function 
-board.forEach(function (whoseTurn, idx){
-    if (idx > 0 && idx <= 6){
-        playerTurn = 1;
-        return playerTurn
+board.forEach(function (boar, idx){
+    if(boar === player1.holes > 0 && player1.holes <= 6){
+        return start;
     }
      else {
-         playerTurn = -1;
-        return playerTurn
-     }
+         return player2.holes
     });
+
 
 function playerOption(){
     if (idx > 0 && idx >= 6){
-       //allow player to pick these options 
+       
     }
      else {
         return playerTurn.p2
      }
     };
 
-function start() {
-      playerTurn = 1;
+
+
+function start(evt) {
+    let clickedHole = evt.target;
+    let clickedHoleIndex = player1.holes;
     
+     
      render()
 }
     
@@ -87,13 +104,20 @@ function finalResult(){}
 function winner(){}
 
 //VIEW 
-function board(){}
-function hole(){}
+// allows user to click on a hole on their side 
+function selectHole( board, player){
+    return 
+}
+
+//allows the distribution of the marbles 
+function hole(){
+   if (playerTurn){
+       for (let i = 1; i < 7; i++)
+   }
+}
 function render( ){
   p1ScoreEl.textContent = scores.p1;
   p2ScoreEl.textContent = scores.p2;
-
-  //do I need to render the intial game board titled let board or based on my results
 
 }
 
