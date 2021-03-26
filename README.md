ddc6e2ede0ceed16b06568201810cda526be8679
Denise Onukwue <donukwue1@gmail.com>
2021-03-26 11:45:21 -0400

    update final

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# mancalagame
+# Mancala: Quarantine Edition 
+
+## Origin: 
+
+### Mancala is so old its precise origins are unknown, but the most reliable evidence exists for mancala having been played 3,600 years ago in Ancient Sudan or Ghan
+
+### This game is a modification of the modern day version. Click this link: https://endlessgames.com/wp-content/uploads/Mancala_Instructions.pdf to read about the traditional method of game play.
+
+## Items:
+
+#### 1 board with 2 rows of 6 holes and 2 mancalas/pits on opposite sides of the board and 48 marbles 
+
+## Rules:
+
+### The game begins with 4 stones located in each circle of the 2 rows. The mancalas on each player's side should be empty. Player 1 will start the game by picking up marbles from a hole from their respective row and moving in a clockwise fashion to deposit one marble into each hole, skipping Player 2's mancala, until they run out of marbles. If Player 1's last marble was placed in their mancala then they get to take another turn. If not, it will revert to Player 2's turn.
+
+## How to Win:
+
+### The first player to reach 10 stones in their mancala will be determined the winner.
+
+## Screenshots :
+
+<img src="/Mancala/imgs/gamestart.png">
+<img src="/Mancala/imgs/gameplay.png">
+<img src="/Mancala/imgs/winscreen.png">
+
+## Technologies Used:
+
+### Bootstrap, HTML, CSS, Javascript 
+
+## Getting Started: 
+
+### Click this link to start! https://denisekathy.github.io/mancalagame/ 
+### **Please note:** This game will only allow you to control both players moves OR you can play with a friend next to you.
+
+## Next Steps/Enhancements: 
+### Player 2 will randomly select their own moves
+### Create an input feature that allows you to change the names of the Players
+### Add a pop up box that you can click to view the rules 
+### Change the numbers to actual marbles and add sounds to stimulate marble movement 
+### Animate the movement of marbles so the game will actually show each marble being dropped off. 
--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-///MODEL/////
-/*----- constants -----*/
-// variables that are not going to be reassigned
 let board = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4];
 const player1 = {
   holes: [1, 2, 3, 4, 5, 6],
@@ -13,22 +10,12 @@ const player2 = {
 };
 
 prompts = ["Player 1 Wins!", "Tie Game!", "Player 2 Wins"];
-// let playerHoles = 14;
-/*----- app's state (variables) -----*/
-//changes that are being tracked /data at a given moment in time
 
-// let scores = 0;
-// let holes = 4;
-// let pits = 0;
-// let winner = null;
 let pickedUpMarbles;
-
-/*----- cached element references -----*/
-//html elements to be manipulated
 
 const p1ScoreEl = document.querySelector("#player1-score");
 const p2ScoreEl = document.querySelector("#player2-score");
-const holes = document.querySelectorAll(".hole"); //need to confirm this is the right syntax
+const holes = document.querySelectorAll(".hole"); 
 const pits = document.querySelectorAll(".mancala");
 
 const p1ResultsEl = document.querySelector("#player1-score");
@@ -68,23 +55,20 @@ pHoles.push(
 );
 
 const prompt = document.querySelector(".winnerPrompt");
-/*----- event listeners -----*/
-//for the holes
+
 holes.forEach((hole) => hole.addEventListener("click", playerTurn));
 
-/*----- event listeners -----*/
-//for the holes
+
 document.querySelectorAll(".col hole").addEventListener
 ("click", start);
-/*----- functions -----*/
+
 function initialize() {
   scores = {
     p1: 0,
     p2: 0,
   };
 
-  // Amount of tokens in each hole at the beginning//
-  let marbles = 4; // 4 marbles per hole
+  let marbles = 4; 
   let pits = 0;
 
   player1Turn = null;
@@ -93,35 +77,22 @@ function initialize() {
 }
 
 function playerTurn(evt) {
-  //When the user clicks on a hole that is not an index of 0 or 7 the game will start based on indexes
-  //   console.log(evt);
+
   let holeSelected = evt.target;
   let holeSelectedIndex = parseInt(holeSelected.id);
 
-  //Store value from selected hole in  pickedUpMarbles variable
   pickUpMarbles = board[holeSelectedIndex];
-  //The hole that is selected will now be set to 0
+ 
   board[holeSelectedIndex] = 0;
 
-  //Deposit marbles into other holes
+  
   for (let i = 1; i <= pickUpMarbles; i++) {
-    console.log("this is i ", i);
-    console.log("this is holeSelectedIndex + i ", holeSelectedIndex + i);
     if (holeSelectedIndex + i > 13) {
       holeSelectedIndex -= holeSelectedIndex + i;
-      console.log("this is holeSelectedIndex ", holeSelectedIndex);
-      // first time through i = 1 and holeSelectedIndex[11] and board[12] += 1
-      // first time through i = 2 and holeSelectedIndex[11] and board[13] += 1
-      // first time through i = 3 and holeSelectedIndex[11] and board[0] += 1
     }
     board[holeSelectedIndex + i] += 1;
-    // console.log("this is i ", i);
-    // console.log("This is holeSel ", holeSelectedIndex);
-    // console.log("This is pickedUp ", pickUpMarbles);
   }
 
-  console.log("board is ", board);
-  //   console.log(board);
   checkWin();
   render();
 }
@@ -129,40 +100,7 @@ function playerTurn(evt) {
 let player1Turn = true;
 let player2Turn = false;
 
-// function spreadMarbles(){
-// Player 1 goes first and can only pick up marbles from indexes 1-6 and not indexes 0 or 7
-//  let player1Turn === true {
-//      if(player1.holes === holeSelectedIndex && holeSelectedIndex !== player2.holes)
-//  }
-
-//  else{
-//      player1Turn === false
-//  };
-// //Player 2 can only pick up marbles from indexes 8-13 and not indexes 0 or 7
-// player2Turn === true {
-//     if(player2.holes === holeSelectedIndex && holeSelectedIndex !== player1.holes)
-// }
-// else {
-//     player2Turn === false
-// };
-
-// };
-
-function depositMarble() {
-  //Based on the number of pickedUpMarbles, deposit one marble into each hole starting from indexes 1-6 except at index 7 for Player 1
-} //Based on the number of pickedUpMarbles, deposit one marble into each hole  starting from indexes 8-13 except at index 0 for Player 2
-//Do both of these in a counter-clockwise fashion
-
-function whoseTurn() {
-  //If player 1's marbles last land in hole with an index of 0, repeat turn
-  //If player 1's marbles last land in holes with indexes of 8-13, then it is Player 2's turn
-  //If player 2's marbles last land in a hole with an index of 7, repeat turn
-  //If player 2's marbles last land in holes with indexes of 1-6, then in is Player 2's turn
-}
-
 function checkWin() {
-  // Winner will be first player to reach 10 in their pit
-  // Compare player 1 score to player 2 score and the larger amount will be the winner
 
   if (board[0] === 10 && board[7] === 10) {
     prompt.textContent = prompts[1];
@@ -171,16 +109,11 @@ function checkWin() {
   } else if (board[7] >= 10 && board[7] > board[0]) {
     prompt.textContent = prompts[2];
   }
-  //Prompts with who is the winner and who is the loser
+  
 }
 
-// console.log(pHoles[0]);
+
 function render() {
-  // for(let i = 0; i < board.length; i++) {
-  //     pHoles[i].textContent = board[i];
-
-  //     };
-
   hole0.textContent = board[0];
   hole1.textContent = board[1];
   hole2.textContent = board[2];
@@ -201,57 +134,3 @@ function render() {
 }
 
 initialize();
-// depositMarbles();
-// pickUpMarbles();
-// whoseTurn();
-// getWin();
-
-//VIEW
-// // allows user to click on a hole on their side
-// function selectHole( board, player){
-//     return
-// }
-
-// //allows the distribution of the marbles
-// function hole(){
-//    if (playerTurn){
-//        for (let i = 1; i < 7; i++)
-//    }
-// }
-//   let holesSelected = function (holeSelectedIndex, pits) {
-//     let i = 0;
-//     let amountOfMarbles = board[startIndex];
-//     let repeat = 0;
-//     let aHole = 0;
-
-//     board[holeSelectedIndex] = 0;
-
-//     //Loop around the holes"arrays" beginning with the one selected and the amount picked up
-//     for (let i = holeSelectedIndex + 1; i < holeSelectedIndex + 1 + amountOfMarbles; i++) {
-//       aHole = i % playerHoles; //go around board
-//       board[aHole] = board[aHole] + 1; // board[Hole] += 1
-//     }
-//     if (aHole === player1.pit) {
-//       let repeat = true;
-//       return repeat;
-//     }
-
-// };
-
-// let boardStart = function () {
-//   for (let i = 0; i < board; i++) {
-//     if (i == player1.pit || i == player2.pit) {
-//       board[i] = 0;
-//     } else {
-//       board[i] = holes;
-//     }
-//   }
-// };
-
-// let pickUpMarbles = function (startIndex) {
-//   for (let i = 0; i < playerHoles; i++) {
-//     let aHole = i + startIndex;
-//     let amountOfMarbles = board[aHole];
-
-//   }
-// }
